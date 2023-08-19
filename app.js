4ca810e06c8b53b304327d345a96d59a8496ada3
ayushnigam099 <ayushnigam95531@gmail.com>
2023-08-19 17:55:24 +0530

    Request / Response

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.end('Hello, this is Ayush Nigam!');
+    const reqq= request.url;
+    switch(reqq)
+    {
+        case ('/'):
+            {
+                response.end('Welcome to my server');
+                break;
+            }
+        case ('/home'):
+        {
+            response.end('Welcome home');
+            break;
+        }
+        case ('/about'):
+            {
+                response.end('Welcome to About Us page');
+                break;
+            }
+        case ('/node'):
+            {
+            response.end('Welcome to my Node Js project');
+            break;
+           }
+           default:
+            {
+                response.end("404 Not Found!");
+            }
+    }
 });
 
 const port = 4000;
 server.listen(port, () => {
-    console.log('AYUSH NIGAM');
+    console.log(`Server started at ${port}`);
 });
